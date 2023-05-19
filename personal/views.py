60ece74603858cb2a1894b3bd9102fe2d24004bb
abcFearey <aa3vil@gmail.com>
2023-05-19 17:05:11 +0200

    Centering and resizing our search bar

--- personal/views.py
+++ personal/views.py
@@ -5,6 +5,12 @@ from blog.models import BlogPost
 
 def home_screen_view(request):
 	context = {}
+
+	query = ""
+	if request.GET:
+		query = request.GET['q']
+		context['query'] = str(query)
+
 	blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
 	context['blog_posts'] = blog_posts
 	return render(request, "personal/home.html", context)
